234d74d5177e06035fb164ac4c19c473ff8d73d2
ALMINA  TANGLAO <tanglaoalms@gmail.com>
2025-01-15 10:42:14 +0800

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { interval } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ng-pipes-demo';
+  presentDate = new Date();
+  time$ = interval(1000).pipe(
+    map(() => new Date())
+  );
+
+  testObject = {
+    name: 'Almina Tanglao',
+    age: 21,
+    food: 'Pizza'
+  };
+
+  price : number = 2000;
+
+  Fruits = ['Apple', 'Orange', 'Grapes', 'Mango', 'Kiwi', 'Pomegranate'];
+
+  decimalNum1: number = 8.7589623;
+  decimalNum2: number = 5.43;
+
+  amount = 123.45;
+  company = 'mina corporation';
+  purchasedOn = '2025-01-15';
 }
