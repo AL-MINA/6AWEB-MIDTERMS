90c2905fab5d975b69c17f31d923f400aa7e1826
mi-na <tanglaoalms@gmail.com>
2025-01-22 09:55:55 +0800

    Update app.component.ts
    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
